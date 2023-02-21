62cc0acb990f77dbdaa515dfdfe916de7af9914b
AlexeyZh <waltafunk@gmail.com>
2023-02-21 12:27:47 +0300

    add exercise 4.2.py

---
+++ exercise 4.2.py
@@ -0,0 +1,39 @@
+small = True or False
+green = True or False
+
+cherry = ['red', small]
+peas = [green, small]
+watermelon= [green, 'big']
+pumpkin = ['yellow', 'big']
+
+if cherry:
+    if green == cherry and small == cherry:
+        print('Green', 'Small')
+    else:
+        print(cherry)
+
+if peas:
+    if green == peas and small == peas:
+        print('Green', 'Small')
+    else:
+        print(peas)
+if watermelon:
+    if green == watermelon and small == watermelon:
+        print('Green', 'Small')
+    else:
+        print(watermelon)
+
+if pumpkin:
+    if green == pumpkin and small == pumpkin:
+        print('Green', 'Small')
+    else:
+        print(pumpkin)
+
+
+
+
+
+
+
+
+
