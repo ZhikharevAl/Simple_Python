4cd0f55e5d91aa47ebb216ffe12b30802e763000
AlexeyZh <waltafunk@gmail.com>
2023-02-21 13:39:23 +0300

    add exercise 5.1.py

---
+++ exercise 5.1.py
@@ -0,0 +1,6 @@
+# 5.1. Напишите с заглавной буквы слово, которое начинается с буквы m:
+song = """When an eel grabs your arm,
+... And it causes great harm,
+... That's - a moray!"""
+
+print(song.replace('moray', 'Moray'))
