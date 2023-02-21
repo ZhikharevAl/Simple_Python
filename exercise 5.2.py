6f7158c722d6b73896f2f4d6df9bb0409d17e6d4
AlexeyZh <waltafunk@gmail.com>
2023-02-21 14:18:16 +0300

    add exercise 5.2.py

---
+++ exercise 5.2.py
@@ -0,0 +1,19 @@
+# #Выведите на экран все вопросы из списка, а также правильные ответы в таком
+# виде:
+# Q: вопрос
+# A: ответ
+questions = [
+        "We don't serve strings around here. Are you a string?",
+        "What is said on Father's Day in the forest?",
+        "What makes the sound 'Sis! Boom! Bah!'?"
+        ]
+answers = [
+        "An exploding sheep.",
+        "No, I'm a frayed knot.",
+        "'Pop!' goes the weasel."
+        ]
+
+
+print(f'Q: {questions[0]}\nA: {answers[0]}')
+print(f'Q: {questions[1]}\nA: {answers[1]}')
+print(f'Q: {questions[2]}\nA: {answers[2]}')
