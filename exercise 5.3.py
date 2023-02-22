08c0a1fbd3e8ba7cb4754863e0ab6879b61039f7
AlexeyZh <waltafunk@gmail.com>
2023-02-22 16:13:28 +0300

    exercise 5.3.py

---
+++ exercise 5.3.py
@@ -0,0 +1,15 @@
+# #Выведите на экран следующее стихотворение, используя старый стиль форматирования.
+# Подставьте в него такие строки: 'roast beef', 'ham', 'head' и 'clam':
+
+
+# My kitty cat likes %s,
+# My kitty cat likes %s,
+# My kitty cat fell on his %s
+# And now thinks he's a %s.
+
+
+print("My kitty cat likes %s," % "roast beef")
+print("My kitty cat likes %s," % "ham")
+print("My kitty cat fell on his %s" % "head")
+print("And now thinks he's a %s." % "clam")
+
