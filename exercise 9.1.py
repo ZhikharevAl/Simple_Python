3f455f8cc811d0650a6548c2de5356ad53131ff2
AlexeyZh <waltafunk@gmail.com>
2023-03-02 21:22:21 +0300

    add exercise 9.11

---
+++ exercise 9.1.py
@@ -0,0 +1,11 @@
+"""
+Определите функцию good(), которая возвращает следующий список:
+['Harry', 'Ron', 'Hermione'].
+"""
+
+
+def good():
+    return ['Harry', 'Ron', 'Hermione']
+
+
+print(good())
