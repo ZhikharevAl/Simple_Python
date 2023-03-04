bf85b25d701082ff3a5efe747713188c87d99c87
AlexeyZh <waltafunk@gmail.com>
2023-03-04 17:13:47 +0300

    add exercise 10.2

---
+++ exercise 10.2.py
@@ -0,0 +1,21 @@
+"""
+Создайте класс Element, имеющий атрибуты объекта name, symbol и number.
+Создайте объект этого класса со значениями 'Hydrogen', 'H' и 1.
+"""
+
+
+class Element:
+    pass
+
+
+element = Element()
+
+name = Element
+symbol = Element
+number = Element
+
+element.name = 'Hydrogen'
+element.symbol = 'H'
+element.number = 1
+
+print(element.name, element.symbol, element.number)
