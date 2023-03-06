5c205f5b94c596c46bea95045cd8c10de5f3e860
AlexeyZh <waltafunk@gmail.com>
2023-03-06 23:00:41 +0300

    add 10.6

---
+++ exercise 10.4-10.8.py
@@ -0,0 +1,45 @@
+import json
+
+"""
+Создайте класс Element, имеющий атрибуты объекта name, symbol и number.
+Создайте объект этого класса со значениями 'Hydrogen', 'H' и 1.
+"""
+
+
+class Element:
+    def __init__(self):
+        self.name = 'Hydrogen'
+        self.symbol = 'H'
+        self.number = 1
+
+    def dump(self):
+        #return self.name, self.symbol, self.number
+        return f"{self.name} {self.symbol} {self.number}"
+
+element = Element()
+
+print(element.name, element.symbol, element.number)
+
+"""
+Создайте словарь со следующими ключами и значениями: 'name': 'Hydrogen',
+'symbol': 'H', 'number': 1. Далее создайте объект с именем hydrogen класса
+Element с помощью этого словаря.
+"""
+
+a = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
+
+hydrogen = Element()
+
+print(json.dumps(hydrogen.__dict__))
+
+"""
+Для класса Element определите метод с именем dump(), который выводит на
+экран значения атрибутов объекта (name, symbol и number). Создайте объект
+hydrogen из этого нового определения и используйте метод dump(), чтобы вывести на экран его атрибуты.
+"""
+
+print(hydrogen.dump())
+
+"""
+
+"""
