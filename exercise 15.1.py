f6457c65f0ce9ef342cd5016547c9f493d0fa6d3
AlexeyZh <waltafunk@gmail.com>
2023-03-08 15:59:44 +0300

    add exercise 15.1 TO DO

---
+++ exercise 15.1.py
@@ -0,0 +1,21 @@
+"""
+Используйте модуль multiprocessing, чтобы создать три отдельных процесса.
+Заставьте каждый из них подождать случайное количество секунд между
+нулем и единицей, вывести текущее время, а затем завершить работу.
+"""
+
+import multiprocessing
+import time
+import os
+
+
+def whoami(what):
+    print("Process %s says: %s" % (os.getpid(), what))
+    os.system('date –u')
+
+if __name__ == "__main__":
+    whoami("I'm the main program")
+    for n in range(4):
+        p = multiprocessing.Process(target=whoami,
+                                    args=("I'm function %s" % n,))
+        p.start()
